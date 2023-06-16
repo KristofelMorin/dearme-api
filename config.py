e84b7a2906093ef71c30adef5f6adebbe6f52013
Kristofel Morin <114991936+KristofelMorin@users.noreply.github.com>
2023-06-16 21:05:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 """Flask configuration variables."""
-from os import environ, path
-
+import os
 from dotenv import load_dotenv
 
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, ".env"))
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, ".env"))
 
-
-class Config:
+class Config(object):
     """Set Flask configuration from .env file."""
 
     # General Config
-    SECRET_KEY = environ.get("SECRET_KEY")
-    FLASK_APP = environ.get("FLASK_APP")
-    FLASK_ENV = environ.get("FLASK_ENV")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    FLASK_APP = os.environ.get("FLASK_APP")
+    FLASK_ENV = os.environ.get("FLASK_ENV")
 
+     
+    basedir = os.path.abspath(os.path.dirname(__file__))
     # Database
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+    SQLALCHEMY_ECHO= False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
