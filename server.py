49d5c3bfe63296bef325c812360e28e202270fad
Kristofel Morin <114991936+KristofelMorin@users.noreply.github.com>
2023-06-16 21:05:06 +0700

    Update server.py

--- server.py
+++ server.py
@@ -3,14 +3,13 @@
 import os
 from flask import Flask
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
-from utils.load_model import*
+from users.models import db
+#from utils.load_model import init
 
-
-db = SQLAlchemy()
+#db = SQLAlchemy()
 mail = Mail()
-
 
 def create_app():
     """Construct the core application."""
@@ -24,30 +23,28 @@ def create_app():
     app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_HOST_PASSWORD")
     app.config["MAIL_USE_TLS"] = False
     app.config["MAIL_USE_SSL"] = True
-
     mail = Mail(app)
 
     app.config.from_object("config.Config")
-
+   
+    db.init_app(app)
+    
     api = Api(app=app)
 
     from users.routes import create_authentication_routes
 
     create_authentication_routes(api=api)
-
-    db.init_app(app)
+    #Initializing database
 
     with app.app_context():
-
         db.create_all()  # Create database tables for our data models
-
         return app
 
-global loaded_model
-load_model = init()
+# global loaded_model
+# loaded_model = init()
 #loaded_model = init()
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="127.0.0.1", port=5000)
-    #app.run(debug=True)
-
+    app.run(host="0.0.0.0", port=5000)
+    #app.run(host="127.0.0.1", port=5000, debug=True)
+    
